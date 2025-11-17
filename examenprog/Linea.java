d4dec246466cd930ddcf71d4ca9491852fdeba73
JhojanGabo <jhojan.g.osco@gmail.com>
2025-11-17 10:03:13 -0400

    Jhojan Gabriel Osco Castaño Examen 2

---
+++ examenprog/Linea.java
@@ -0,0 +1,5 @@
+package examenprog;
+
+public class Linea {
+
+}
---
+++ examenprog/MiTeleferico.java
@@ -0,0 +1,10 @@
+package examenprog;
+
+public class MiTeleferico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
